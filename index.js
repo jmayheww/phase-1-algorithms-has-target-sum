c1156766643bcfbe758141623a9ab867d8c2291e
Joshua <joshsmayhew@gmail.com>
2022-07-17 14:44:15 +0700

    commit tree 48a1c684f48c5403e8cb695a1ac454ca0c8345be

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
-/* 
-  Write the Big O time complexity of your function here
+/* I am trying to create a function that finds two
+integers within an array and evaluates whether they amount to a target value,
+passed as the second argument in the function. For example, if I have an
+array [1, 2, 3, 4] and a target of 6, my function should return true because 2 + 4 amount to 6.
 */
 
-/* 
-  Add your pseudocode here
+function hasTargetSum(array, target) {
+  // iterate through the array to find first num
+  for (let i = 0; i < array.length; i++) {
+    // find complementary number that adds together with first number to equal target
+    // (comp = target - num)
+    const complement = target - array[i];
+    // iterate through rest of array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any number is the complement
+      // if so, return true
+      if (array[j] === complement) return true;
+    }
+    // if i reach the end of the array, return false
+  }
+  return false;
+}
+
+hasTargetSum([1, 4, 7, 3], 5);
+console.log("hasTargetSum([1, 4, 7, 3], 5);: ", hasTargetSum([1, 4, 7, 3], 5));
+
+/*
+  Runtime: O(n^2)
+  Space: O(n)
 */
 
 /*
-  Add written explanation of your solution here
+  Create function that assesses values of elements in an array
+  and evaluates whether any two elements add up to the sum of target.
+  The function initially assigns a variable, i, to iterates through the array
+  in order to find the first number. From there, we can determine the complementary
+  number as the target number minus the first number, array[i]. To find whether the array
+  has the complementary number, we iterate through the array again with new variable, j.
+  if j equates to the complementary number, then we return true. Otherwise, we iterate through to
+  the end of the array, which equates to false.
 */
 
 // You can run `node index.js` to view these console logs
